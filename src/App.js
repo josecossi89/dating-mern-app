e8b6fa667262405e6e4d16eaac78939f2fae7a4d
Jose Cossi <josecossib@gmail.com>
2022-08-20 15:53:14 -0300

    component SwipeButtons added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/Header";
 import DatingCards from "./components/DatingCards";
+import SwipeButtons from "./components/SwipeButtons";
 function App() {
   return (
     <div className="app">
       <Header />
       <DatingCards />
+      <SwipeButtons />
     </div>
   );
 }
